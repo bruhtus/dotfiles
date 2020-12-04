5d75f286e48c1d5f414e290d4c1568126bd2c3d7
bruhtus <hibruhtus@gmail.com>
2020-12-05 03:32:11 +0700

    Add compatibility to display results in vim -R

--- git-status-checker.py
+++ git-status-checker.py
@@ -6,8 +6,8 @@ dir_list = [dirname for dirname in os.listdir(os.getcwd()) if os.path.isdir(dirn
 for dirname in dir_list:
     os.chdir(dirname)
     #print(f"\033[1m{dirname}\033[0m") #bold text in terminal
-    os.system(f"echo -e '\033[1m{dirname}\033[0m'") #another approach
-    #os.system('pwd')
+    #os.system(f"echo -e '\033[1m{dirname}\033[0m'") #another approach
+    os.system('pwd')
 
     if os.path.exists('.gitmodules') == True:
         os.system('git status -s')
