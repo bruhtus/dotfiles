6c1d565d7c984d549c313883a097bf4a181eb3c7
bruhtus <hibruhtus@gmail.com>
2021-01-03 05:31:00 +0700

    Grep primary and connected monitor from xrandr

--- .i3/startxrandr.sh
+++ .i3/startxrandr.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-xrandr --output HDMI-2 --auto --left-of eDP-1
+xrandr --output $(xrandr | grep primary | awk '{print $1}') --auto --right-of $(xrandr | grep -v primary | grep " connected" | awk '{print $1}')
