ae9d74332a26fcedae46fe1f5911743d19365b6b
bruhtus <hibruhtus@gmail.com>
2021-01-07 06:32:17 +0700

    Add xrandr auto detection for single and multiple monitor

--- .i3/startxrandr.sh
+++ .i3/startxrandr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xrandr --output $(xrandr | grep primary | awk '{print $1}') --auto --right-of $(xrandr | grep -v primary | grep " connected" | awk '{print $1}')
+[ $(xrandr | grep " connected" | wc -l) == 1 ] && xrandr --auto || xrandr --auto --output $(xrandr | grep primary | awk '{print $1}') --auto --right-of $(xrandr | grep -v primary | grep " connected" | awk '{print $1}')
